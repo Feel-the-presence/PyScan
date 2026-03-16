868bed1b32e6d0463974f52e2b140a096e4529ff
Feel-the-presence <nechiporenkom12@gmail.com>
2026-03-16 17:46:18 +0200

    Add files via upload

---
+++ RefactoredScanner/NetworkScan.py
@@ -0,0 +1,87 @@
+from scapy.all import *
+import socket
+
+class NetworkScanner:
+    def __init__(
+        self, 
+        target_ip: str, 
+        ports_to_scan: list[int], 
+        interface = None, 
+        local_ip = None, 
+        gateway_ip = None
+    ) -> None:
+
+        auto_iface, auto_local_ip, auto_gateway = conf.route.route("0.0.0.0")
+
+        self.interface = interface or auto_iface
+        self.local_ip = local_ip or auto_local_ip
+        self.gateway_ip = gateway_ip or auto_gateway
+
+        self.target_ip: str = target_ip
+        self.ports_to_scan: list[int] = ports_to_scan
+        self.open_ports: list[int] = []
+        """
+            target_ip (str): IPv4-адрес цели для сканирования.
+            ports_to_scan (list[int]): Список портов для проверки.
+        """
+
+    def arp_scan(self, count: int = 5) -> dict:
+
+        network_prefix = self.gateway_ip.rsplit('.', 1)[0]
+        target_network = f"{network_prefix}.0/24"
+
+        arp_frame = [Ether(dst = "ff:ff:ff:ff:ff:ff")/ARP(pdst = target_network)] * count
+        answered, unanswered = srp(arp_frame, iface = self.interface, timeout = 3, verbose = False)   
+        self.route_tab = {}
+
+        for sent, received in answered:
+            if received.psrc != self.local_ip:
+                self.route_tab[received.psrc] = received.hwsrc
+        
+        return self.route_tab
+        
+
+
+    def scan_ports(self) -> None:
+
+        if not self.ports_to_scan:
+            return
+
+        packet = IP(dst = self.target_ip)/TCP(dport = self.ports_to_scan, flags = "S")
+        answered, unanswered = sr(packet, timeout = 2, verbose = False)
+
+        for info, received_packet in answered:
+            if received_packet.haslayer(TCP):
+                if received_packet.getlayer(TCP).flags == "SA":
+                    open_port = info[TCP].dport
+                    if open_port not in self.open_ports:
+                        self.open_ports.append(open_port)
+
+                        rst_packet = IP(dst = self.target_ip)/TCP(dport = open_port, flags = "R")
+                        send(rst_packet, verbose = 0)
+
+    def grab_banner(self, port: int) -> str:
+
+        #----------------------------
+        #Забить на метод(временно) или позже доработать для исключений (FTP, HTTP, etc.)
+        #----------------------------
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(2.0)
+
+        try:
+            s.connect((self.target_ip, port))
+            probe = b"SERVER\r\n\r\n"
+            s.send(probe)
+
+            response_bytes = s.recv(1024)
+
+            banner = response_bytes.decode("utf-8", errors="ignore").strip()
+            return banner if banner else "No banner"
+
+        except socket.timeout:
+            return "Timed-out"
+        except Exception as e:
+            return f"Connection error: {e}"
+        finally:
+            s.close()
---
+++ RefactoredScanner/main.py
@@ -0,0 +1,22 @@
+import NetworkScan
+
+main_ports = [135, 139, 445]
+
+if __name__ == "__main__":
+
+    my_scanner = NetworkScan.NetworkScanner("192.168.0.106", main_ports)
+
+    choice = input("Choose an option (1-portScan, 2-grabBanner, 3-arpscan):    ")
+
+    if choice == "1":
+        my_scanner.scan_ports()
+
+        print("Open ports: ", my_scanner.open_ports)
+
+    elif choice == "2":
+        banner = my_scanner.grab_banner(445)
+
+        print("Info: ", banner)
+
+    elif choice == "3":
+        my_scanner.arp_scan()
